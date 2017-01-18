ef25f7f2a955e5e149abf65de899d4eed9a1e737
rchatsiri <rchatsiri@gmail.com>
2017-01-17 21:32:33 -0800

    On branch elf_structure_asm_feature  Changes to be committed: 	modified:   README.md 	modified:   src/nc/arch/arm/CMakeLists.txt 	modified:   src/nc/common/CMakeLists.txt 	modified:   src/nc/core/CMakeLists.txt 	modified:   src/nc/input/elf/CMakeLists.txt 	modified:   src/nc/test/ReadELFTest.hpp  Changes not staged for commit: 	modified:   src/filestructure (modified content, untracked content)

--- src/nc/test/ReadELFTest.hpp
+++ src/nc/test/ReadELFTest.hpp
@@ -4,14 +4,16 @@
 #include <nc/core/Driver.h>
 #include <nc/core/Context.h>
 
-class ReadELFTest : public testing::Test{
+class ReadELFTest : public testing::Test
+{
 
-public:
+    public:
 
-nc::core::Context context;
+        nc::core::Context context;
 };
 
-TEST_F(ReadELFTest, simple_read_elf){
-	folly::fbstring filename("/home/rchatsiri/workspacereverseengineer/android-file");
-	nc::core::Driver::parse(context, filename);
+TEST_F(ReadELFTest, simple_read_elf)
+{
+    folly::fbstring filename("/home/rchatsiri/workspacereverseengineer/android-file");
+    nc::core::Driver::parse(context, filename);
 }
